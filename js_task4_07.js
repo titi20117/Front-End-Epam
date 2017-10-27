29f4172420f41ed7b65492005030abc5c620bbb2
titi20117 <fernandmoudio1@gmail.com>
2017-10-27 09:36:55 +0300

    Update js_task4_07.js

--- js_task4_07.js
+++ js_task4_07.js
@@ -1,4 +1,11 @@
-function myFunction(params1, params2, params3) {
-    
+function myFunction(params1, params2, params3){ 
+    var myString1, myString2, myString3;
+    var myString = params1.split(" ");
+    myString1 = myString.slice(0, params3 + 1);
+    myString2 = myString.slice(params3 + 1);
+    myString3 = myString2.join(" ");
+    myString1.push(params2);
+    myString1.push(myString3);
+    return console.log(myString1.join(" "));
 }
-myFunction("I am cool", "very", 1);
+myFunction("I am cool like a baby", "sexy", 4);

96a845b3235b3af1b1b609c1dcd834bbe76b8a41
Fernand <moudio.fernand@yandex.ru>
2017-10-30 21:25:37 +0300

    Task6

--- dropElements.js
+++
@@ -1,5 +0,0 @@
-function dropElements(arr, func) {
-    return console.log(arr.slice(arr.findIndex(func) >=0 ? arr.findIndex(func): arr.length, arr.length));
-}
-
-dropElements([1, 6, 2, 3, 4], function(n) {return n>=1;});
--- factoriel.js
+++ factoriel.js
@@ -1,11 +1,12 @@
 
 function factorialize(num) {
-    if (num === 0 || num === 1) {
-      return num = 1;
-    } else {
-      num = num * factorialize(num -1);
-    }
-    return num;
+  if (num === 0 || num === 1) {
+    return num = 1;
   }
-  
-  console.log(factorialize(5));
+  if (num != 1) {
+    num = num * factorialize(num -1);
+  }
+  return num;
+}
+
+console.log(factorialize(5));
